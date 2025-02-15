33832c57f5847df322163d3ac66866a1b9ae97cb
SA-10125 <surajacharya2005@gmail.com>
2025-02-16 03:32:42 +0530

    Tried gallery, but css etc didnt work. loading time slow and backend is fine. consider making a thing where the image link (local/global) entered in the model while creating/saving a new gallery object automatically converts into byte64

--- My_try_Eclub/gallery/views.py
+++ My_try_Eclub/gallery/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from homepage.models import gallery
 
 def render_gallery(request):
-    return HttpResponse('gallery will come here')
+    return render(request,'temp_gallery.html',{'gallery':gallery.objects.all()})
 
-#have the gallery load using a loop and preferably from byte64 encrypt, but careful, might slow the website further.
+#have loaded the images as byte64 encrypted strings as it eleminates the need to store and get the images from the db/server.
+
+#THE CSS ISNT WORKING RIGHT
+#It is significantly slow though it is rendering the same images it did in homepage.
+#this might be due to their size in the current css. fix
+#eventually hope to get server side rendering or maybe we should go back to the old system.
