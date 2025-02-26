e5ea75dacfc86b3208f19c21f88c94800e74e7bb
irongirl101 <141424562+irongirl101@users.noreply.github.com>
2025-02-26 21:56:36 +0530

    changed styles (in html direct), to match og website

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",  // Scans all HTML templates in the templates folder
+    "./static/src/**/*.css",  // Scans CSS files in static/src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
